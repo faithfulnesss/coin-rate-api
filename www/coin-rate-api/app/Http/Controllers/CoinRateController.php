f16fb4a9a6f5c53bccd9a7e91d63b1b7912e1cc2
faithfulnesss <torfik1med@gmail.com>
2023-05-26 07:41:17 +0000

    Implement CoinRateService (class for handling HTTP request for CoinMarketCap API) and CoinRateController (Controller to fetch the data from the CoinRateService and bind with GET /api/rate route

---
+++ www/coin-rate-api/app/Http/Controllers/CoinRateController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Config;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use App\Services\CoinRateService;
+
+class CoinRateController extends Controller
+{
+    //
+    private $coinRateService;
+
+    public function __construct(CoinRateService $coinRateService)
+    {
+        $this->coinRateService = $coinRateService;
+    }
+    
+    public function getRate(Request $request)
+    {
+        $rate = $this->coinRateService->getRate();
+        
+        if(empty($rate)) {
+            
+            return response()->json(['msg' => 'Failed to fetch the BTC-UAH rate.','error' => true], Response::HTTP_BAD_REQUEST);            
+    
+        }
+
+        return $rate;
+    }
+}
---
+++ www/coin-rate-api/app/Services/CoinRateService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Config;
+
+class CoinRateService
+{
+    protected $apiKey;
+    protected $apiUri;
+
+    public function __construct()
+    {
+
+        $this->apiKey = Config::get('services.cmp.token');
+        $this->apiUri = 'https://pro-api.coinmarketcap.com/v1/';
+    
+    }
+
+    public function getRate()
+    {
+        $response = Http::withHeaders([
+            'X-CMC_PRO_API_KEY' => $this->apiKey,
+        ])->get($this->apiUri . 'cryptocurrency/quotes/latest', [
+            'symbol' => 'BTC',
+            'convert' => 'UAH',
+        ]);
+    
+        $data = @json_decode($response->getBody()->getContents(), true);
+        
+        $rate = $data['data']['BTC']['quote']['UAH']['price'] ?? null;
+
+        return $rate;
+        // }
+
+        // return null;
+    }
+}
