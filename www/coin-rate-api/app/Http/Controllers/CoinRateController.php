b2659253dec76bc5dbd6ca62082f95f43bca68fd
faithfulnesss <torfik1med@gmail.com>
2023-05-27 20:15:00 +0000

    fix the mistake of controller returning rate value instead of http response

--- www/coin-rate-api/app/Http/Controllers/CoinRateController.php
+++ www/coin-rate-api/app/Http/Controllers/CoinRateController.php
@@ -28,6 +28,6 @@ class CoinRateController extends Controller
             return response()->json(['msg' => 'Failed to fetch the BTC-UAH rate.'], Response::HTTP_BAD_REQUEST);            
         }
 
-        return $rate;
+        return response()->json(['rate' => $rate], Response::HTTP_OK);
     }
 }
