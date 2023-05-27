bfe24048170f8cdd3b30bdf55cbb5d10d6ea8a73
faithfulnesss <torfik1med@gmail.com>
2023-05-27 19:16:34 +0000

    Push the Subscription model that handles the logic of read/write subscription list from the json file and inherits the Notiable trait to make subscriptions usable from the Notification Facade

---
+++ www/coin-rate-api/app/Models/Subscription.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Collection;
+use Illuminate\Notifications\Notifiable;
+
+
+class Subscription extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = ['email', 'subscription_date'];
+
+    /**
+     * Get the email address where notification should be sent.
+     *
+     * @return string
+     */
+    public function routeNotificationForMail()
+    {
+        return $this->email;
+    }
+
+    public static function getAllSubscriptions(): Collection
+    {
+        $contents = File::get(storage_path('app/subscriptions.json'));
+        $subscriptions = collect(json_decode($contents, true));
+
+        // Convert each array item to a Subscription object
+        $subscriptions = $subscriptions->map(function ($item) {
+            return new self($item);
+        });
+
+        return $subscriptions;
+    }
+
+    // public static function getAllSubscriptions(): Collection
+    // {
+    //     $contents = File::get(storage_path('app/subscriptions.json'));
+    //     $subscriptions = collect(json_decode($contents, true));
+        
+    //     return $subscriptions ?: collect();
+    // }
+
+    public static function create(array $attributes = []): bool
+    {
+        $subscription = new self($attributes);
+
+        return $subscription->save();
+    }
+
+    public function save(array $options = [])
+    {
+        $subscriptions = self::getAllSubscriptions();
+        
+        if ($subscriptions->firstWhere('email', $this->email)){
+            return false;
+        }  
+    
+        $subscriptions->push($this);
+    
+        File::put(storage_path('app/subscriptions.json'), $subscriptions->toJson());
+    
+        return true;
+    }    
+}
