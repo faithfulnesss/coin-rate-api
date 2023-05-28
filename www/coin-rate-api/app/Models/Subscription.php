7a451fdbc151e839872642cf11b25dcd489dbab9
faithfulnesss <torfik1med@gmail.com>
2023-05-28 15:16:27 +0000

    Remove the excess logic from the Subscription model, so model will implement only two static methods loadSubscriptions and saveSubscriptions

--- www/coin-rate-api/app/Models/Subscription.php
+++ www/coin-rate-api/app/Models/Subscription.php
@@ -9,6 +9,13 @@ use Illuminate\Support\Collection;
 use Illuminate\Notifications\Notifiable;
 
 
+/**
+ * Class Subscription
+ * 
+ * Represents a subscription model and provides methods
+ * to load and save collections of Subscription objects.
+ * 
+ */
 class Subscription extends Model
 {
     use HasFactory, Notifiable;
@@ -24,10 +31,19 @@ class Subscription extends Model
     {
         return $this->email;
     }
-
+    
+    /**
+     * Static method to load the subscriptions from the storage file
+     *
+     * @return Collection|Subscription[]
+     */
     public static function loadSubscriptions(): Collection
     {
+        // Read the contents of the subscriptions.json file using Storage facade
         $contents = Storage::get('subscriptions.json');
+
+        // Decode the JSON contents into a collection 
+        // or use an empty array if decoding fails
         $subscriptions = collect(@json_decode($contents, true) ?? []);
 
         // Convert each array item to a Subscription object
@@ -35,21 +51,20 @@ class Subscription extends Model
             return new self($item);
         });
 
+        // Return collection of Subscription model object
         return $subscriptions;
     }
 
+    /**
+     * Static method to save the collection of Subscriptions in the storage file
+     *
+     * @param  Collection|Subscription[] $subscriptions
+     * @return bool
+     */
     public static function saveSubscriptions(Collection $subscriptions): bool
     {
-        // $subscriptions = self::getAllSubscriptions();
-        
-        // if ($subscriptions->firstWhere('email', $this->email)){
-        //     return false;
-        // }  
-    
-        // $subscriptions->push($this);
-    
+        // Save the collection of Subscription model objects as a JSON
+        // in the subscription.json file
         return Storage::put('subscriptions.json', $subscriptions->toJson());
-    
-        // return true;
     }    
 }
